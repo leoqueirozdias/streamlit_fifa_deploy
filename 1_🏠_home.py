6f1ee3cc81e00fb252c4a8ebea3e81bbcd665e19
leoqueirozdias <63917757+leoqueirozdias@users.noreply.github.com>
2024-08-19 22:38:57 -0300

    Update 1_🏠_home.py

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -13,10 +13,12 @@ if 'data' not in st.session_state:
 st.markdown('# FIFA23 OFFICIAL DATASET!⚽')
 st.sidebar.markdown('Desenvolvido por [Leonardo.Dias]')
 
+btn = st.link_button(
+    'Acesse dados no Kaggle',
+    "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data"
+)
 btn = st.button('Acesse dados no Kaggle')
-if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
-    
+
 st.markdown(
     """
     O conjunto de dados
